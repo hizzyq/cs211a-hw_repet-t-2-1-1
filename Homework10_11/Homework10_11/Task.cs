57793e53dc337bfca339156a1a9b08213c0e7685
Алексей <alexkernel05@gmail.com>
2025-05-07 08:50:04 +0300

    Create Task class

---
+++ Homework10_11/Homework10_11/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10_11
+{
+    class Task
+    {
+        public string СondTask {  get; set; }
+        public string AnsTask { get; set; }
+
+        public Task(string сondTask, string ansTask)
+        {
+            СondTask = сondTask;
+            AnsTask = ansTask;
+        }
+    }
+}
